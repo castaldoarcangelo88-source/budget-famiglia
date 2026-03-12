5a98c7b215815f677a149bd37b19470566c07a0c
castaldoarcangelo88-source <castaldoarcangelo88@gmail.com>
2026-03-12 19:20:15 +0100

    Add firebase.js for Firebase integration

---
+++ firebase.js
@@ -0,0 +1,47 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { 
+  getDatabase, 
+  ref, 
+  push, 
+  set, 
+  get, 
+  child, 
+  onValue 
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8Et2dRp8Ds9nh5VEbJ8rPIyRGRFGOY",
+  authDomain: "budget-famiglia-4477e.firebaseapp.com",
+  databaseURL: "https://budget-famiglia-4477e-default-rtdb.firebaseio.com",
+  projectId: "budget-famiglia-4477e",
+  storageBucket: "budget-famiglia-4477e.appspot.com",
+  messagingSenderId: "915933633260",
+  appId: "1:915933633260:web:c873f3b3649c9e01a5d92d",
+  measurementId: "G-W4EZD3XD7"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+
+// riesporto funzioni utili così le importi in index.html
+export {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  ref,
+  push,
+  set,
+  get,
+  child,
+  onValue
+};
