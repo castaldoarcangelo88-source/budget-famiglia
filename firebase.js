3f7cdf6d1dbd73caf3155c4446c0fa4337de2cd2
castaldoarcangelo88-source <castaldoarcangelo88@gmail.com>
2026-03-12 19:58:36 +0100

    Update Firebase configuration wRestore correct Firebase API Key and configurationith new API keys

--- firebase.js
+++ firebase.js
@@ -19,13 +19,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB8Et2dRp8Ds9nh5VEbJ8rPIyRGRFGOY",
+    apiKey: "AIzaSyB8E0t2dRpB0ds9nh5VEBJ8rPIyfGRFgOY",
     authDomain: "budget-famiglia-4477e.firebaseapp.com",
     databaseURL: "https://budget-famiglia-4477e-default-rtdb.firebaseio.com",
     projectId: "budget-famiglia-4477e",
-    storageBucket: "budget-famiglia-4477e.appspot.com",
-    messagingSenderId: "915933633260",
-    appId: "1:915933633260:web:c873f3b3649c9e01a5d92d",
+    storageBucket: "budget-famiglia-4477e.firebasestorage.app",
+    messagingSenderId: "915933306200",
+    appId: "1:915933306200:web:c873f3b3649ce901a5d92d",
     measurementId: "G-W4EZD3XD7"
 };
 
